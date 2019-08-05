a2b8d283d1ac2eb52de07d4379a7a7ca702cc1c6
andrew-dorrycott <andrew.dorrycott@gmail.com>
2019-08-04 21:30:20 -0500

    Added Missing URL Input & Replaced Flask Globals with Config for api_key

--- clients/giphy.py
+++ clients/giphy.py
@@ -1,26 +1,38 @@
 # Third party imports
-import flask
 import requests
+
+# Application imports
+import config
 
 
 class Client(object):
     def make_request(
-        self, method="GET", payload=None, params=None, headers=None
+        self, url, method="GET", payload=None, params=None, headers=None
     ):
         if not params:
             params = {}
         if not headers:
             headers = {}
 
-        headers["api_key"] = flask.globals.giphy["api_key"]
+        headers["api_key"] = config.giphy["api_key"]
         params["rating"] = "g"  # Forced G rating
 
         response = requests.request(
-            method=method, data=payload, params=params, headers=headers
+            url=url,
+            method=method,
+            data=payload,
+            params=params,
+            headers=headers,
         )
         return response.json()
 
     def search(self, query, limit=25, offset=0, lang="en"):
         return self.make_request(
-            params={"q": query, "limit": limit, "offset": offset, "lang": lang}
+            url=config.giphy["search_endpoint"],
+            params={
+                "q": query,
+                "limit": limit,
+                "offset": offset,
+                "lang": lang,
+            },
         )
