db88360bc58f2f5e720b9fb4596997e5b3e7fcb6
andrew-dorrycott <andrew.dorrycott@gmail.com>
2019-08-05 17:29:26 -0500

    Updated Docs & Added Ability to Get Gifs by ID

--- clients/giphy.py
+++ clients/giphy.py
@@ -9,6 +9,23 @@ class Client(object):
     def make_request(
         self, url, method="GET", payload=None, params=None, headers=None
     ):
+        """
+        Centralized handler of all requests to GIPHY
+
+        :param url: URL being requested
+        :type url: str
+        :param method: GET/POST/PUT/DELETE
+        :type method: str
+            :default: GET
+        :param payload: Data or json being sent
+        :type payload: str
+        :param params: Dict of parameters being sent
+        :type params: dict
+        :param headers: Additional headers being sent
+        :type headers: dict
+        :returns: Results from GIPHY
+        :rtype: dict
+        """
         if not params:
             params = {}
         if not headers:
@@ -27,6 +44,23 @@ class Client(object):
         return response.json()
 
     def search(self, query, limit=25, offset=0, lang="en"):
+        """
+        Search GIPHY's APIs
+
+        :param query: What to search for
+        :type query: str
+        :param limit: Amount of items to return per page
+        :type limit: int
+            :default: 25
+        :param offset: Where to start in the total results
+        :type offset: int
+            :default: 0
+        :param lang: What language to send back
+        :type lang: str
+            :default: en
+        :returns: Results from GIPHY
+        :rtype: dict
+        """
         return self.make_request(
             url=config.giphy["search_endpoint"],
             params={
@@ -35,4 +69,18 @@ class Client(object):
                 "offset": offset,
                 "lang": lang,
             },
+        )
+
+    def get(self, gifid):
+        """
+        Search GIPHY's APIs
+
+        :param query: What to search for
+        :type query: str
+        :param limit: Amount of items to return per page
+        :returns: Results from GIPHY
+        :rtype: dict
+        """
+        return self.make_request(
+            url="{}/{}".format(config.giphy["get_endpoint"], gifid)
         )
