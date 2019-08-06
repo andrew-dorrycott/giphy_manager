ea4bb0ca09b1913e9a8ca93bd4a9c418180f940d
andrew-dorrycott <andrew.dorrycott@gmail.com>
2019-08-06 02:15:37 -0500

    Added Missing Link Between Bookmarks and Categories

---
+++ models/bookmark_xref_categories.py
@@ -0,0 +1,57 @@
+# Third party imports
+import sqlalchemy
+
+# Application imports
+from models import database
+
+
+class BookmarkXrefCategory(database.Base):
+    __tablename__ = "bookmark_xref_categories"
+
+    # Composite key
+    bookmark_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey("bookmarks.id"),
+        primary_key=True,
+    )
+    category_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey("categories.id"),
+        primary_key=True,
+    )
+
+    def __init__(self, **kwargs):
+        """
+        Initializer
+
+        :returns: Nothing
+        :rtype: None
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        """
+        Custom String representation
+
+        :returns: String representation of the data
+        :rtype: str
+        """
+        message = (
+            "<BookmarkXrefCategory(user_id='{user_id}', "
+            + "giphy_id='{giphy_id}', category_id='{category_id}')>"
+        )
+        return message.format(**self.__dict__)
+
+    def to_dict(self):
+        """
+        Custom Dictionary/Json-able representation
+
+        :returns: Dict representation of the data
+        :rtype: dict
+        """
+        return {
+            "user_id": self.user_id,
+            "giphy_id": self.giphy_id,
+            "category_id": self.category_id,
+        }
