9adb10bf1fedda43c390be520d1a91e1d0847a1b
andrew-dorrycott <andrew.dorrycott@gmail.com>
2019-08-06 05:24:47 -0500

    Updated to View All Saved and Favorited Bookmarks

--- static/view.js
+++ static/view.js
@@ -1,66 +1,136 @@
 
-function buildUX() {
-  var search = document.getElementById("search");
-  search.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      document.getElementById("submit").click();
-    }
-  });
-}
 
-function renderResults(data) {
-  var table = "";
-
-  if (data["results"]) {
-    table += "<table><tr>";
-
-    for (var key in data["results"][0]) {
-      table += ("<th align=\"center\">" + key + "</th>");
-    }
-    table += "</tr>";
-
-    for (var x in data["results"]) {
-      table += "<tr>";
-      for (var key in data["results"][x]) {
-        table += ("<td align=\"center\">" + data["results"][x][key] + "</td>");
-      }
-      table += "</tr>";
-    }
-    table += "</table>";
-
-    result_data.innerHTML = table;
-  }
-
-  return table;
-}
-
-
-function executeSearch() {
+function addCategory(gifid) {
   var xhttp = new XMLHttpRequest();
+  // When the Add button is clicked, add the category to the bookmark
+  var categories = document.getElementById("categories_" + gifid);
+  var new_category = document.getElementById("categories_for_" + gifid);
+  var category_id = new_category.options[new_category.selectedIndex].value;
+  var category_name = new_category.options[new_category.selectedIndex].text;
+
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(this.responseText);
-      if ("error" in data) {
-        document.getElementById("result_count").innerHTML = "Results: 0";
-        document.getElementById("result_data").innerHTML = data["error"];
-      }
-      else {
-        document.getElementById("result_count").innerHTML = "Results: " + data["count"];
-        result_data = document.getElementById("result_data")
-        result_data.innerHTML = renderResults(data);
-      }
-    }
-    else {
-      document.getElementById("result_count").innerHTML = "Bad request";
-      document.getElementById("result_data").innerHTML = "Bad request";
+      categories.innerHTML += (
+        "<div id=\"cat_" + gifid + "_" + category_id + "\" align=\"left\">"
+        + "<button type=\"button\" onclick=\"removeCategory('" + gifid + "', "
+        + category_id + ")\">Remove</button>" + "&nbsp;&nbsp;&nbsp; "
+        + category_name + "</div>"
+      );
     }
   };
 
-  document.getElementById("result_count").innerHTML = "Loading...";
-  document.getElementById("result_data").innerHTML = "Loading...";
-  xhttp.open(
-    "GET", "/search/" + escape(document.getElementById("search").value), true
-  );
+  xhttp.open("GET", "/add_categories/" + gifid + "/" + category_id);
+  xhttp.send();
+}
+
+
+function removeCategory(gifid, category_id) {
+  var xhttp = new XMLHttpRequest();
+  // When the Add button is clicked, add the category to the bookmark
+  var category = document.getElementById("cat_" + gifid + "_" + category_id);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Remove the category from the div
+      category.innerHTML = "";
+    }
+  };
+
+  xhttp.open("GET", "/remove_categories/" + gifid + "/" + category_id);
+  xhttp.send();
+}
+
+
+function loadAndRenderCategories(gifid) {
+  var xhttp = new XMLHttpRequest();
+  var categories = document.getElementById("categories_" + gifid);
+
+  xhttp.onreadystatechange = function() {
+    var output = "";
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      // Give a drop down to add categories
+      output = "<select id=\"categories_for_" + gifid + "\">";
+
+      for (var x in data) {
+        output += (
+          "<option value=\"" + data[x]["id"] + "\">" + data[x]["name"]
+          + "</option>"
+        );
+      }
+
+      output += "</select>";
+      output += (
+        "<button type=\"button\" onclick=\"addCategory('" + gifid
+        + "')\">Add</button>"
+      );
+    }
+    categories.innerHTML = output;
+  };
+
+  xhttp.open("GET", "/get_categories");
+  xhttp.send();
+}
+
+
+function toggleBookmark(gifid) {
+  var xhttp = new XMLHttpRequest();
+  var already_saved = document.getElementById("saved_" + gifid);
+  var bookmark_button = document.getElementById("bookmark_" + gifid);
+  var favorite_button = document.getElementById("favorite_" + gifid);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      if (already_saved.value == "false") {
+        bookmark_button.innerHTML = "Remove";
+        already_saved.value = true;
+        loadAndRenderCategories(gifid);
+      }
+      else {
+        bookmark_button.innerHTML = "Save";
+        favorite_button.innerHTML = "Favorite";
+        already_saved.value = false;
+        document.getElementById("favorited_" + gifid).value = false;
+      }
+    }
+  };
+
+  if (already_saved.value == "false") {
+    xhttp.open("GET", "/save_gif_by_id/" + escape(gifid), true);
+  }
+  else {
+    xhttp.open("GET", "/remove_gif_by_id/" + escape(gifid), true);
+  }
+  xhttp.send();
+}
+
+
+function toggleFavorite(gifid) {
+  var xhttp = new XMLHttpRequest();
+  var already_saved = document.getElementById("favorited_" + gifid);
+  var bookmark_button = document.getElementById("bookmark_" + gifid);
+  var favorite_button = document.getElementById("favorite_" + gifid);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      if (already_saved.value == "false") {
+        bookmark_button.innerHTML = "Remove";
+        favorite_button.innerHTML = "Unfavorite";
+        already_saved.value = true;
+        loadAndRenderCategories(gifid);
+      }
+      else {
+        favorite_button.innerHTML = "Favorite";
+        already_saved.value = false;
+      }
+    }
+  };
+
+  if (already_saved.value == "false") {
+    xhttp.open("GET", "/favorite_gif_by_id/" + escape(gifid), true);
+  }
+  else {
+    xhttp.open("GET", "/unfavorite_gif_by_id/" + escape(gifid), true);
+  }
   xhttp.send();
 }
